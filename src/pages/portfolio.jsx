c921618d7a8e3fa8b0d837ac9b985532ea3d4a4a
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-04-17 12:40:32 -0600

    added more to portfolio section

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -7,6 +7,10 @@ import WeatherDashboardImage from '../assets/cardimages/weatherdash.jpg';
 import WorkdaySchedulerImage from '../assets/cardimages/WorkDayScheduler.jpg';
 import PasswordGeneratorImage from '../assets/cardimages/PasswordGenerator.jpg';
 import TaskMasterImage from '../assets/cardimages/TaskMasterImage.jpg';
+import BookSearchEngineImage from '../assets/cardimages/BookSearchEngineImage.jpg';
+import CodeQuizImage from '../assets/cardimages/CodeQuiz.jpg';
+import NoteTakerImage from '../assets/cardimages/NoteTaker.jpg';
+
 
 
 const projects = [
@@ -39,6 +43,13 @@ const projects = [
     githubLink: 'https://github.com/huntrreed/Weather-Dashboard',
   },
   {
+    title: 'Book Search Engine',
+    subtitle: 'GraphQL/MERN Application',
+    imageUrl: BookSearchEngineImage,
+    projectLink: 'https://book-search-engine-zoyf.onrender.com/',
+    githubLink: 'https://github.com/huntrreed/book-search-engine?tab=readme-ov-file',
+  },
+  {
     title: 'Workday Scheduler',
     subtitle: 'Third Party APIs',
     imageUrl: WorkdaySchedulerImage,
@@ -46,11 +57,25 @@ const projects = [
     githubLink: 'https://github.com/huntrreed/Work-Day-Scheduler',
   },
   {
+    title: 'Note Taker',
+    subtitle: 'Express.js',
+    imageUrl: NoteTakerImage,
+    projectLink: 'https://note-taker-huntrreed-05349f067777.herokuapp.com/',
+    githubLink: 'https://github.com/huntrreed/Note-Taker',
+  },
+  {
     title: 'Password Generator',
     subtitle: 'Javascript',
     imageUrl: PasswordGeneratorImage,
     projectLink: 'https://huntrreed.github.io/Password-Generator/',
     githubLink: 'https://github.com/huntrreed/Password-Generator',
+  },
+  {
+    title: 'Code Quiz',
+    subtitle: 'Web APIs',
+    imageUrl: CodeQuizImage,
+    projectLink: 'https://huntrreed.github.io/Code-Quiz/',
+    githubLink: 'https://github.com/huntrreed/Code-Quiz',
   },
   // Note to me - can add more projects here
 ];
