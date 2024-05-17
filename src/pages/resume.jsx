47bbc2f3c31050e37a41266be7084ae01bfdda44
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-05-17 17:02:50 +0200

    resume page reformatted

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,12 +1,23 @@
+// src/pages/Resume.js
+import React from 'react';
+import '../assets/css/Resume.css';
+
 const Resume = () => {
-    return (
-      <div className="resumeContent">
-        <h1 className="resume"></h1>
-        <div className="resumeImage">
-          <iframe src="../public/pdf/HunterReedResume.pdf" width="100%" height="500px"/>
+  return (
+    <div className="resume-page">
+      <div className="resume-content">
+        <h1 className="resume-title">Hunter Reed Resume</h1>
+        <div className="resume-iframe">
+          <iframe
+            src="../public/pdf/HunterReedResume.pdf"
+            width="100%"
+            height="500px"
+            title="Hunter Reed Resume"
+          />
         </div>
       </div>
-    );
-  };
-  
-  export default Resume;
+    </div>
+  );
+};
+
+export default Resume;
