ba4bb412fb35152f8f37f363bf66875d0860f238
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-05-17 16:58:32 +0200

    contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,7 @@
+// src/pages/Contact.js
 import React from 'react';
 import { Form, Input, Button, notification } from 'antd';
+import '../assets/css/Contact.css';
 
 const ContactPage = () => {
   const onFinish = async (values) => {
@@ -7,14 +9,14 @@ const ContactPage = () => {
     for (const [key, value] of Object.entries(values)) {
       form.append(key, value);
     }
-    
+
     try {
       const response = await fetch('https://formspree.io/f/xpzvkwpz', {
         method: 'POST',
         body: form,
         headers: {
-          'Accept': 'application/json'
-        }
+          'Accept': 'application/json',
+        },
       });
 
       if (response.ok) {
@@ -37,40 +39,42 @@ const ContactPage = () => {
   };
 
   return (
-    <div className="contact-page" style={{ backgroundImage: 'url(your-background-image.jpg)' }}>
-      <Form
-        name="contact"
-        className="contact-form"
-        onFinish={onFinish}
-        layout="vertical"
-      >
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true, message: 'Please input your name!' }]}
+    <div className="contact-page">
+      <div className="form-container">
+        <Form
+          name="contact"
+          className="contact-form"
+          onFinish={onFinish}
+          layout="vertical"
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          label="Email"
-          rules={[{ required: true, message: 'Please input your email!', type: 'email' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="message"
-          label="Message"
-          rules={[{ required: true, message: 'Please input your message!' }]}
-        >
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Send Message
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: 'Please input your name!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[{ required: true, message: 'Please input your email!', type: 'email' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="message"
+            label="Message"
+            rules={[{ required: true, message: 'Please input your message!' }]}
+          >
+            <Input.TextArea />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Send Message
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
