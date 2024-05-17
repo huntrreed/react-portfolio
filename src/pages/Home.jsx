c740051df7c2a87e19759438f6140326816dce40
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-05-17 16:43:34 +0200

    homepage functional with changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,25 @@
 // src/pages/Home.js
 import React from 'react';
 import '../assets/css/Home.css';
+import ProfileImage from '../assets/me.jpg'; 
 
 const Home = () => {
   return (
     <div className="home">
       <div className="header-content">
-        <h1>I'm Hunter Reed.</h1>
-        <p>I am a new full stack web developer.</p>
-        <p>I recently finished the University of Denver's coding bootcamp and am excited to start working in the web development world.</p>
-        <div className="social-links">
-          <a href="#facebook"><i className="fa fa-facebook"></i></a>
-          <a href="#linkedin"><i className="fa fa-linkedin"></i></a>
-          <a href="#instagram"><i className="fa fa-instagram"></i></a>
-          <a href="#github"><i className="fa fa-github"></i></a>
+        <div className="text-content">
+          <h1>I'm Hunter Reed.</h1>
+          <p>I am a new full stack web developer.</p>
+          <p>I recently finished the University of Denver's coding bootcamp and am excited to start working in the web development world.</p>
+          <div className="social-links">
+            <a href="#facebook"><i className="fa fa-facebook"></i></a>
+            <a href="#linkedin"><i className="fa fa-linkedin"></i></a>
+            <a href="#instagram"><i className="fa fa-instagram"></i></a>
+            <a href="#github"><i className="fa fa-github"></i></a>
+          </div>
+        </div>
+        <div className="image-content">
+          <img src={ProfileImage} alt="Profile" />
         </div>
       </div>
     </div>
