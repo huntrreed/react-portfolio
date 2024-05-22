6dd0f3c25a229f45e8a86c0f010354f7a7edae92
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-05-22 19:57:14 +0200

    header color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,8 @@ const Home = () => {
           <p>I am a new full stack web developer.</p>
           <p>I recently finished the University of Denver's coding bootcamp and am excited to start working in the web development world.</p>
           <div className="social-links">
-            <a href="#facebook"><i className="fa fa-facebook"></i></a>
-            <a href="#linkedin"><i className="fa fa-linkedin"></i></a>
-            <a href="#instagram"><i className="fa fa-instagram"></i></a>
-            <a href="#github"><i className="fa fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/hunter-reed-8282b7127/"><i className="fa fa-linkedin"></i></a>
+            <a href="https://github.com/huntrreed"><i className="fa fa-github"></i></a>
           </div>
         </div>
         <div className="image-content">
