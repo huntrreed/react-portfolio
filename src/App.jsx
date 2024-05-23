524525f62603ef1584b80dfdf2474f67fdc53d4e
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-05-23 13:47:54 +0200

    trying to deploy to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
+import Contact from './pages/contact';
 import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
+import Resume from './pages/resume';
 import Header from './components/Header';
 
 function App() {
