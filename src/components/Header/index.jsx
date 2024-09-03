3de73756aca537058e3502ebf7cac91ea1a9177d
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-09-03 16:45:06 -0600

    new logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaLinkedin, FaGithub, FaBars } from 'react-icons/fa';
 import '../../assets/css/Header.css';
+import logo from '../../assets/logo.png'; // Adjust the path to your logo image
 
 const Header = () => {
   const location = useLocation();
@@ -14,8 +15,18 @@ const Header = () => {
   return (
     <>
       <nav id="nav-wrap">
-        <div className="site-name">
-          Hunter Reed
+        <div className="social-icons">
+          <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+        </div>
+        <div className="site-logo">
+          <Link to="/">
+            <img src={logo} alt="Site Logo" />
+          </Link>
         </div>
         <div className="hamburger" onClick={toggleModal}>
           <FaBars />
@@ -24,11 +35,6 @@ const Header = () => {
           <li className={location.pathname === '/' ? 'current' : ''}>
             <Link className="smoothscroll" to="/">
               Home
-            </Link>
-          </li>
-          <li className={location.pathname === '/resume' ? 'current' : ''}>
-            <Link className="smoothscroll" to="/resume">
-              Resume
             </Link>
           </li>
           <li className={location.pathname === '/portfolio' ? 'current' : ''}>
@@ -42,23 +48,12 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <div className="social-icons">
-          <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin />
-          </a>
-          <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
-            <FaGithub />
-          </a>
-        </div>
       </nav>
       {isModalOpen && (
         <div className="modal" onClick={toggleModal}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <Link className="smoothscroll" to="/" onClick={toggleModal}>
               Home
-            </Link>
-            <Link className="smoothscroll" to="/resume" onClick={toggleModal}>
-              Resume
             </Link>
             <Link className="smoothscroll" to="/portfolio" onClick={toggleModal}>
               Work
